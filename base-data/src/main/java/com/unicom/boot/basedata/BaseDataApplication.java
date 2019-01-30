c58b0f2dd07a11d61562b031cbbd585b6f4dc63c
denghao <dengh20@chinaunicom.cn>
2019-01-30 11:07:53 +0800

    创建spring-boot-study

---
+++ base-data/src/main/java/com/unicom/boot/basedata/BaseDataApplication.java
@@ -0,0 +1,14 @@
+package com.unicom.boot.basedata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseDataApplication.class, args);
+	}
+
+}
+
---
+++ base-mvc/src/main/java/com/unicom/boot/basemvc/BaseMvcApplication.java
@@ -0,0 +1,17 @@
+package com.unicom.boot.basemvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author dengh
+ */
+@SpringBootApplication
+public class BaseMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseMvcApplication.class, args);
+	}
+
+}
+
